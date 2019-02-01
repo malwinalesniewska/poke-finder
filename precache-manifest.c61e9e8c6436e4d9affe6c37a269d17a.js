3f3f5c3281619fb78ba954b1ecc41d6f0c12382d
Malwina Leśniewska <malwina.lesniewska@gmail.com>
2019-02-01 15:16:37 +0100

    Updates

---
+++ precache-manifest.c61e9e8c6436e4d9affe6c37a269d17a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "76e143d1f8217a68ab6e872a5ccc8343",
+    "url": "/poke-finder/static/media/pokemon-logo.76e143d1.png"
+  },
+  {
+    "revision": "8a6a1496db9cfec2ce2abf0a8e527aad",
+    "url": "/poke-finder/static/media/pokeball_icon_no_image.8a6a1496.png"
+  },
+  {
+    "revision": "a6a61af25a7f0e3d5179486940bd3406",
+    "url": "/poke-finder/static/media/pokeball-1594373_960_720edited.a6a61af2.png"
+  },
+  {
+    "revision": "84c4e8c1a24b84bece745d2dcd2a5aa8",
+    "url": "/poke-finder/static/media/crying_pikachu.84c4e8c1.gif"
+  },
+  {
+    "revision": "0721f61a55ed5ce45d9840531cbbb532",
+    "url": "/poke-finder/static/media/Pikachu_(Winking).0721f61a.png"
+  },
+  {
+    "revision": "e44ff0f474d3d8760d2f",
+    "url": "/poke-finder/static/js/runtime~main.e44ff0f4.js"
+  },
+  {
+    "revision": "148675a0eebdc3549cec",
+    "url": "/poke-finder/static/js/main.148675a0.chunk.js"
+  },
+  {
+    "revision": "40682a0680893bb5f021",
+    "url": "/poke-finder/static/js/1.40682a06.chunk.js"
+  },
+  {
+    "revision": "148675a0eebdc3549cec",
+    "url": "/poke-finder/static/css/main.de8730c6.chunk.css"
+  },
+  {
+    "revision": "aa7c8cd8e7fb4ab8e0507adc2dab865f",
+    "url": "/poke-finder/index.html"
+  }
+];
